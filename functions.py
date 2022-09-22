8c843072052fd21398231455a2be6ceb429ebe90
zian999 <12528594+zian999@users.noreply.github.com>
2022-09-22 16:22:57 -0400

    add commands that can check and disply schedules of regular, bankara, and coop games

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+from io import BytesIO
 import requests
-import re
+import discord
+from PIL import Image
+from bot_data import *
 
 def user_agent():
     headers = {
@@ -27,8 +30,8 @@ def get_schedule(p1, p2):
 def handle(entry):
     st = datetime.strptime(entry['start_time'], "%Y-%m-%dT%H:%M:%S%z")
     et = datetime.strptime(entry['end_time'], "%Y-%m-%dT%H:%M:%S%z")
-    rule_name = entry['rule']['name']
     if 'stages' in entry:
+        rule_name = entry['rule']['name']
         stages = [s['name'] for s in entry['stages']]
         return [[st, et], rule_name, stages]
     if 'weapons' in entry:
@@ -46,3 +49,83 @@ def timediff(earlier_time, later_time):
         s.insert(0, '0')
     s[3] = str(round(float(s[3]), 1))
     return s
+
+def embed_content(entry):
+    data = handle(entry)
+    t = data[0]
+    if datetime.now(tz = t[0].tzinfo) > t[0]:
+        tdelta = timediff(datetime.now(tz = t[1].tzinfo), t[1])
+        embed = discord.Embed(
+            title = f"Ends in {tdelta[0]} days, {tdelta[1]} hours, {tdelta[2]} mins."
+        )
+    else:
+        tdelta = timediff(datetime.now(tz = t[0].tzinfo), t[0])
+        embed = discord.Embed(
+            title = f"Starts in {tdelta[0]} days, {tdelta[1]} hours, {tdelta[2]} mins."
+        )
+    if 'stages' in entry:
+        stage_id1 = stage_ids[data[2][0]]
+        stage_id2 = stage_ids[data[2][1]]
+        embed.add_field(
+            name = "~ RULE ~",
+            value = rule_CN_names[rule_ids[data[1]]],
+            inline = False
+        )
+        embed.add_field(
+            name = "~ STAGES ~",
+            value = stage_CN_names[stage_id1] + '\n' + stage_CN_names[stage_id2],
+            inline = False
+        )
+        file = discord.File(embed_stage_image(stage_id1, stage_id2), filename = "o.png")
+        embed.set_image(url="attachment://o.png")
+        return [file, embed]
+    if 'weapons' in entry:
+        weapon_id1 = weapon_ids[data[2][0]]
+        weapon_id2 = weapon_ids[data[2][1]]
+        weapon_id3 = weapon_ids[data[2][2]]
+        weapon_id4 = weapon_ids[data[2][3]]
+        stage_id = stage_ids[data[1]]
+        embed.add_field(
+            name = "~ WEAPONS ~",
+            value = (
+                weapon_CN_names[weapon_id1] + '\n'
+                + weapon_CN_names[weapon_id2] + '\n'
+                + weapon_CN_names[weapon_id3] + '\n'
+                + weapon_CN_names[weapon_id4]
+            ),
+            inline = False
+        )
+        embed.add_field(
+            name = "~ STAGE ~",
+            value = stage_CN_names[stage_id],
+            inline = False
+        )
+        embed.set_image(url=stage_images[stage_id])
+        return embed
+
+def create_image(url1, url2):
+    img1 = read_image(url1)
+    img2 = read_image(url2)
+    img = merge_image(img1, img2)
+    return img
+
+def read_image(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    return img
+
+def merge_image(im1, im2):
+    w = max(im1.size[0], im2.size[0])
+    h = im1.size[1] + im2.size[1]
+    im = Image.new("RGBA", (w, h))
+    im.paste(im1)
+    im.paste(im2, (0, im1.size[1]))
+    return im
+
+def embed_stage_image(id1, id2):
+    im = create_image(stage_images[id1], stage_images[id2])
+    b = BytesIO()
+    im.save(b, "PNG")
+    b.seek(0)
+    return b
+
---
+++ sp3bot.py
@@ -0,0 +1,191 @@
+import discord
+from discord.ext import commands
+from functions import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+description = '''
+A Discord bot for my Splatoon 3 server.
+It can tell you the current and next stages of regular, bankara, and coop games, 
+or list the next N games on the schedule of regular, bankara, and coop games.
+'''
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.command(description='Examples: ?turf | ?turf 3')
+async def turf(ctx, n = 2):
+    if n < 1:
+        messageVar = '`N` requested is too small!'
+        await ctx.reply(content = messageVar)
+        return
+    data = get_schedule('regular', 'schedule')
+    if data is None:
+        await ctx.reply('Failed in getting the data!')
+        return
+    elif n > len(data):
+        await ctx.reply('`N` requested is too large!')
+        return
+    messageVar = f'''
+    **⤋⤋⤋ TURF WAR SCHEDULE (REQUESTED N = {n}) ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    for i in range(n):
+        embedVar = embed_content(data[i])
+        await ctx.send(file = embedVar[0], embed = embedVar[1])
+
+@bot.command(description='Examples: ?bankara open | ?bankara open 3 | ?bankara challenge 3')
+async def bankara(ctx, format = None, n = 2):
+    if n < 1:
+        messageVar = '`N` requested is too small!'
+        await ctx.reply(content = messageVar)
+        return
+    if format is None:
+        messageVar = '**Please specify a BANKARA FORMAT!**'
+        await ctx.reply(content = messageVar)
+        return
+    elif format == "open":
+        data = get_schedule('bankara-open', 'schedule')
+        if data is None:
+            await ctx.reply('Failed in getting the data!')
+            return
+        elif n > len(data):
+            await ctx.reply('`N` requested is too large!')
+            return
+        messageVar = f'''
+        **⤋⤋⤋ BANKARA OPEN SCHEDULE (REQUESTED N = {n}) ⤋⤋⤋**
+        '''
+        await ctx.reply(content = messageVar)
+        for i in range(n):
+            embedVar = embed_content(data[i])
+            await ctx.send(file = embedVar[0], embed = embedVar[1])
+    elif format == "challenge":
+        data = get_schedule('bankara-challenge', 'schedule')
+        if data is None:
+            await ctx.reply('Failed in getting the data!')
+            return
+        elif n > len(data):
+            await ctx.reply('`N` requested is too large!')
+            return
+        messageVar = f'''
+        **⤋⤋⤋ BANKARA CHALLENGE SCHEDULE (REQUESTED N = {n}) ⤋⤋⤋**
+        '''
+        await ctx.reply(content = messageVar)
+        for i in range(n):
+            embedVar = embed_content(data[i])
+            await ctx.send(file = embedVar[0], embed = embedVar[1])
+    else:
+        messageVar = '**wrong BANKARA FORMAT!**'
+        await ctx.reply(content = messageVar)
+        return
+
+@bot.command(description='Examples: ?salmonrun | ?salmonrun 3')
+async def salmonrun(ctx, n = 2):
+    if n < 1:
+        messageVar = '`N` requested is too small!'
+        await ctx.reply(content = messageVar)
+        return
+    data = get_schedule('coop-grouping-regular', 'schedule')
+    if data is None:
+        await ctx.reply('Failed in getting the data!')
+        return
+    elif n > len(data):
+        await ctx.reply('`N` requested is too large!')
+        return
+    messageVar = f'''
+    **⤋⤋⤋ SALMON RUN SCHEDULE (REQUESTED N = {n}) ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    for i in range(n):
+        embedVar = embed_content(data[i])
+        await ctx.send(embed = embedVar)
+
+@bot.command(description='Example: ?now')
+async def now(ctx):
+    data = [None] * 4
+    data[0] = get_schedule('regular', 'now')
+    data[1] = get_schedule('bankara-open', 'now')
+    data[2] = get_schedule('bankara-challenge', 'now')
+    data[3] = get_schedule('coop-grouping-regular', 'now')
+    if None in data:
+        await ctx.reply('Failed in getting the data!')
+        return
+    messageVar = f'''
+    **⤋⤋⤋ CURRENT TURF WAR STAGES ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    embedVar = embed_content(data[0][0])
+    await ctx.send(file = embedVar[0], embed = embedVar[1])
+    messageVar = f'''
+    **⤋⤋⤋ CURRENT BANKARA OPEN STAGES ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    embedVar = embed_content(data[1][0])
+    await ctx.send(file = embedVar[0], embed = embedVar[1])
+    messageVar = f'''
+    **⤋⤋⤋ CURRENT BANKARA CHALLENGE STAGES ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    embedVar = embed_content(data[2][0])
+    await ctx.send(file = embedVar[0], embed = embedVar[1])
+    messageVar = f'''
+    **⤋⤋⤋ CURRENT SALMON RUN STAGE AND WEAPONS ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    embedVar = embed_content(data[3][0])
+    await ctx.send(embed = embedVar)
+    
+@bot.command(description='Example: ?next')
+async def next(ctx):
+    data = [None] * 4
+    data[0] = get_schedule('regular', 'next')
+    data[1] = get_schedule('bankara-open', 'next')
+    data[2] = get_schedule('bankara-challenge', 'next')
+    data[3] = get_schedule('coop-grouping-regular', 'next')
+    if None in data:
+        await ctx.reply('Failed in getting the data!')
+        return
+    messageVar = f'''
+    **⤋⤋⤋ NEXT TURF WAR STAGES ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    embedVar = embed_content(data[0][0])
+    await ctx.send(file = embedVar[0], embed = embedVar[1])
+    messageVar = f'''
+    **⤋⤋⤋ NEXT BANKARA OPEN STAGES ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    embedVar = embed_content(data[1][0])
+    await ctx.send(file = embedVar[0], embed = embedVar[1])
+    messageVar = f'''
+    **⤋⤋⤋ NEXT BANKARA CHALLENGE STAGES ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    embedVar = embed_content(data[2][0])
+    await ctx.send(file = embedVar[0], embed = embedVar[1])
+    messageVar = f'''
+    **⤋⤋⤋ NEXT SALMON RUN STAGE AND WEAPONS ⤋⤋⤋**
+    '''
+    await ctx.reply(content = messageVar)
+    embedVar = embed_content(data[3][0])
+    await ctx.send(embed = embedVar)
+
+
+@bot.command()
+async def tt(ctx):
+    pass
+
+
+
+bot.run(os.getenv('BOT_TOKEN'))
